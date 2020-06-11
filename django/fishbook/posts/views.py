9163a4854764195d9960786f903b5ed97a5a55a2
박정훈 <kangtak6291@naver.com>
2020-06-11 13:48:11 +0900

    post using forms.py

--- django/fishbook/posts/views.py
+++ django/fishbook/posts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
 
 def index(request):
     return render(request, 'posts/index.html')
@@ -13,5 +14,15 @@ def login(request):
     return render(request, 'posts/login.html')
 
 def new(request):
-    return render(request, 'posts/new.html')
+    context = {
+        'form': PostForm()
+    }
+    return render(request, 'posts/new.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return redirect(goFishing)    
 # Create your views here.
